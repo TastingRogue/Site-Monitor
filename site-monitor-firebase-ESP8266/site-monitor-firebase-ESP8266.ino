9907d8f7936ed11c6f3a35aaa7da9cedb30d690f
TastingRogue <irvingsamuel1@hotmail.com>
2018-06-13 01:28:29 -0500

    First Site Monitor Update

---
+++ site-monitor-firebase-ESP8266/site-monitor-firebase-ESP8266.ino
@@ -0,0 +1,43 @@
+// FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "site-monitor-01.firebaseio.com/"
+#define FIREBASE_AUTH "bGZn3Dbxpxt8nIiRGQhf1FmiQgZSjpho2oSvbgG6"
+#define WIFI_SSID "TOTALPLAY_A027B3"
+#define WIFI_PASSWORD "2WD43T2C4W"
+
+int ledPower = 5;
+
+void setup() {
+  //Setup code
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  pinMode(ledPower, OUTPUT);
+}
+
+void loop() {
+  //Main code
+  int ledStatus = Firebase.getInt("ledStatus");
+  Serial.println(ledStatus);
+  if(ledStatus){
+    digitalWrite(ledPower, HIGH); 
+  }
+  else{
+    digitalWrite(ledPower, LOW);
+  }
+}
---
+++ web-site-monitor/script.js
@@ -0,0 +1,36 @@
+// Initialize Firebase
+ var config = {
+	apiKey: "AIzaSyBjTGkYBr8DlxZLATlkEsi9geEMK_DldZY",
+    authDomain: "site-monitor-01.firebaseapp.com",
+    databaseURL: "https://site-monitor-01.firebaseio.com",
+    projectId: "site-monitor-01",
+    storageBucket: "site-monitor-01.appspot.com",
+    messagingSenderId: "612482407497"
+ };
+ firebase.initializeApp(config);
+
+ $(document).ready(function(){
+ 	var database = firebase.database();
+ 	var ledStatus;
+
+ 	database.ref().on("value", function(snap){
+ 		ledStatus = snap.val().ledStatus;
+ 		if (ledStatus){
+ 			$(".lightStatus").text("The light is on");
+ 		}
+ 		else{
+ 			$(".lightStatus").text("The light is off");
+ 		}
+ 	});
+ 	$(".lightButton").click(function(){
+ 		var firebaseRef = firebase.database().ref().child("ledStatus");
+ 		if (led){
+ 			firebaseRef.set(0);
+ 			ledStatus = 0;
+ 		}
+ 		else{
+ 			firebaseRef.set(1);
+ 			ledStatus = 1;
+ 		}
+ 	});
+ });
